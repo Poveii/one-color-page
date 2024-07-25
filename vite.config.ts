bf086d5cc61493ee2de1398084374309926ccaaf
Pablo Gabriel <79858234+Poveii@users.noreply.github.com>
2024-07-25 13:56:08 +0000

    build: Add `vite-plugin-pwa` with basic config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: "#000000",
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
