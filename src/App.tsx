eb03ccb8e26e8ecae4dc848e717845c721ca1115
Pablo Gabriel <yellowtutors@gmail.com>
2024-07-28 10:25:25 -0300

    fix: Improve mouse movement logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, MouseEvent, useState } from "react"
 import {
   Card,
   CardContent,
   CardTitle,
 } from "@/components/ui/card"
 
+type MouseMoveType = MouseEvent<HTMLDivElement> & { target: Element }
+
 function App() {
   const [color, setColor] = useState("#FFFFFF")
-  const [isMouseActive, setIsMouseActive] = useState(true)
+  const [mouseActive, setMouseActive] = useState(false)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setColor(e.target.value)
   }
 
-  function handleMouseMovement() {
-    setTimeout(() => {
-      setIsMouseActive(false)
-    }, 5000);
-    setIsMouseActive(true)
+  let removeMouseTimeout: ReturnType<typeof setTimeout>
+  function handleMouseMovement(event: MouseMoveType) {
+    setMouseActive(true)
+
+    event.target.classList.remove("cursor-none")
+    event.target.classList.add("group")
+
+    const THREE_SECONDS = 3000;
+    (function () {
+      clearTimeout(removeMouseTimeout)
+      removeMouseTimeout = setTimeout(() => {
+        event.target.classList.remove("group")
+        event.target.classList.add("cursor-none")
+      }, THREE_SECONDS)
+    })()
   }
 
   return (
     <div
-      className={"h-screen grid place-content-center relative " + (isMouseActive ? "group" : "cursor-none")}
+      className="h-screen grid place-content-center relative"
       style={{ backgroundColor: color }}
       onMouseMove={handleMouseMovement}
     >
       <h1
-        className="font-bold text-2xl opacity-0 invisible transition-all group-hover:opacity-100 group-hover:visible"
+        className={"font-bold text-2xl opacity-0 invisible transition-all" + (mouseActive ? " group-hover:opacity-100 group-hover:visible" : " !visible !opacity-100")}
       >
         One Color Page
       </h1>
 
-      <Card className="absolute right-8 bottom-8 p-8 space-y-4 dark opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all">
+      <Card className={"absolute right-8 bottom-8 p-8 space-y-4 dark opacity-0 invisible transition-all" + (mouseActive ? " group-hover:opacity-100 group-hover:visible" : " !visible !opacity-100")}>
         <CardTitle>Escolha uma cor</CardTitle>
         <CardContent className="p-0">
           <input 
